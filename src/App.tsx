42da27616ea6648335630cf0ca2327bc00422280
Yuancheng Wu <johnwu0202@gmail.com>
2023-04-03 14:27:55 -0700

    Add basic page transitions

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,59 @@
-import logo from "./logo.svg";
+import { animated, useTransition } from "@react-spring/web";
+import { useLocation, Route, Routes, Link } from "react-router-dom";
 import "./App.css";
+import { MouseEventHandler, useState } from "react";
+
+const backward = {
+  from: { opacity: 0, transform: "translate3d(100%,0,0)" },
+  enter: { opacity: 1, transform: "translate3d(0%,0,0)" },
+  leave: { opacity: 0, transform: "translate3d(-50%,0,0)" },
+};
+
+const forward = {
+  from: { opacity: 1, transform: "translate3d(-50%,0,0)" },
+  enter: { opacity: 1, transform: "translate3d(0%,0,0)" },
+  leave: { opacity: 0, transform: "translate3d(100%,0,0)" },
+};
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  const [reverse, setReverse] = useState(false);
+  const location = useLocation();
+  const transitions = useTransition(
+    location.pathname,
+    reverse ? backward : forward
   );
+
+  return transitions((props, location) => (
+    <animated.div style={props}>
+      <Routes location={location}>
+        <Route path="/" element={<A onClick={() => setReverse(true)} />} />
+        <Route
+          path="lesson/friday-night"
+          element={<A onClick={() => setReverse(true)} />}
+        />
+        <Route
+          path="lesson/friday-night/history"
+          element={<B onClick={() => setReverse(false)} />}
+        />
+      </Routes>
+    </animated.div>
+  ));
 }
 
 export default App;
+
+const A = ({ onClick }: { onClick: MouseEventHandler<HTMLAnchorElement> }) => (
+  <div style={{ background: "lightpink" }}>
+    <Link to="/lesson/friday-night/history" onClick={onClick}>
+      A
+    </Link>
+  </div>
+);
+
+const B = ({ onClick }: { onClick: MouseEventHandler<HTMLAnchorElement> }) => (
+  <div style={{ background: "lightblue" }}>
+    <Link to="/lesson/friday-night/" onClick={onClick}>
+      B
+    </Link>
+  </div>
+);
