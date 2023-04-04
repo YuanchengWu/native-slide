5ba44e9de54d09feab9a6bb10599ac9632d5b466
Yuancheng Wu <johnwu0202@gmail.com>
2023-04-03 18:14:24 -0700

    Add drag interactions

--- src/App.tsx
+++ src/App.tsx
@@ -1,59 +1,76 @@
-import { animated, useTransition } from "@react-spring/web";
-import { useLocation, Route, Routes, Link } from "react-router-dom";
+import { animated, config, useSpring } from "@react-spring/web";
+import { useDrag } from "@use-gesture/react";
 import "./App.css";
-import { MouseEventHandler, useState } from "react";
-
-const backward = {
-  from: { opacity: 0, transform: "translate3d(100%,0,0)" },
-  enter: { opacity: 1, transform: "translate3d(0%,0,0)" },
-  leave: { opacity: 0, transform: "translate3d(-50%,0,0)" },
-};
-
-const forward = {
-  from: { opacity: 1, transform: "translate3d(-50%,0,0)" },
-  enter: { opacity: 1, transform: "translate3d(0%,0,0)" },
-  leave: { opacity: 0, transform: "translate3d(100%,0,0)" },
-};
+import { useWindowWidth } from "./hooks/useWindowWidth";
 
 function App() {
-  const [reverse, setReverse] = useState(false);
-  const location = useLocation();
-  const transitions = useTransition(
-    location.pathname,
-    reverse ? backward : forward
+  const width = useWindowWidth();
+
+  const [{ x }, api] = useSpring(() => ({ x: width }), [width]);
+
+  const open = ({ canceled }: { canceled: boolean }) => {
+    // when cancel is true, it means that the user passed the upwards threshold
+    // so we change the spring config to create a nice wobbly effect
+    api.start({
+      x: 0,
+      immediate: false,
+      config: canceled ? config.wobbly : config.stiff,
+    });
+  };
+
+  const close = (velocity = 0) => {
+    api.start({
+      x: width,
+      immediate: false,
+      config: { ...config.stiff, velocity },
+    });
+  };
+
+  const bind = useDrag(
+    ({ last, velocity: [vx], direction: [dx], movement: [mx], canceled }) => {
+      // when the user releases the sheet, we check whether it passed
+      // the threshold for it to close, or if we reset it to its open positino
+      if (last) {
+        mx > width * 0.5 || (vx > 0.5 && dx > 0)
+          ? close(vx)
+          : open({ canceled });
+      }
+      // when the user keeps dragging, we just move the sheet according to
+      // the cursor position
+      else api.start({ x: mx, immediate: true });
+    },
+    {
+      from: () => [0, x.get()],
+      filterTaps: true,
+      bounds: { top: 0 },
+      rubberband: true,
+    }
   );
 
-  return transitions((props, location) => (
-    <animated.div style={props}>
-      <Routes location={location}>
-        <Route path="/" element={<A onClick={() => setReverse(true)} />} />
-        <Route
-          path="lesson/friday-night"
-          element={<A onClick={() => setReverse(true)} />}
-        />
-        <Route
-          path="lesson/friday-night/history"
-          element={<B onClick={() => setReverse(false)} />}
-        />
-      </Routes>
-    </animated.div>
-  ));
+  const display = x.to((px) => (px < width ? "block" : "none"));
+
+  return (
+    <div>
+      <div
+        style={{ background: "lightpink" }}
+        onClick={() => open({ canceled: false })}
+      >
+        A
+      </div>
+      <animated.div
+        {...bind()}
+        style={{
+          display,
+          right: `calc(-100vw + ${width}px)`,
+          x,
+          background: "lightblue",
+        }}
+        onClick={() => close(0)}
+      >
+        B
+      </animated.div>
+    </div>
+  );
 }
 
 export default App;
-
-const A = ({ onClick }: { onClick: MouseEventHandler<HTMLAnchorElement> }) => (
-  <div style={{ background: "lightpink" }}>
-    <Link to="/lesson/friday-night/history" onClick={onClick}>
-      A
-    </Link>
-  </div>
-);
-
-const B = ({ onClick }: { onClick: MouseEventHandler<HTMLAnchorElement> }) => (
-  <div style={{ background: "lightblue" }}>
-    <Link to="/lesson/friday-night/" onClick={onClick}>
-      B
-    </Link>
-  </div>
-);
---
+++ src/hooks/useWindowWidth.ts
@@ -0,0 +1,18 @@
+import { useState, useEffect } from "react";
+
+export function useWindowWidth(): number {
+  const [windowWidth, setWindowWidth] = useState<number>(window.innerWidth);
+
+  useEffect(() => {
+    function handleResize() {
+      setWindowWidth(window.innerWidth);
+    }
+
+    window.addEventListener("resize", handleResize);
+    handleResize();
+
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  return windowWidth;
+}
